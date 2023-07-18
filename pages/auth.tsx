74e2fe96ff6ce1d3a920a2b2acb0bdcb6ea52362
ricardomadeira93 <ricardomadeira1993@gmail.com>
2023-07-18 13:22:32 +0100

    NextAuth setup

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,17 +1,17 @@
-import { useCallback, useState } from "react";
-import Image from "next/image";
-import Input from "@/components/Input";
+import { useCallback, useState } from 'react';
+import Image from 'next/image';
+import Input from '@/components/Input';
 
 const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
-  const [variant, setVariant] = useState("login");
+  const [variant, setVariant] = useState('login');
 
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) =>
-      currentVariant === "login" ? "register" : "login"
+      currentVariant === 'login' ? 'register' : 'login'
     );
   }, []);
 
@@ -19,57 +19,57 @@ const Auth = () => {
     // Auth Background
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       {/* Auth Container */}
-      <div className="bg-black w-full h-full lg:bg-opacity-70">
+      <div className='bg-black w-full h-full lg:bg-opacity-70'>
         {/* Navigation */}
-        <nav className="py-5 px-12">
-          <Image src="/images/logo.png" width={100} height={12} alt="Logo" />
+        <nav className='py-5 px-12'>
+          <Image src='/images/logo.png' width={100} height={12} alt='Logo' />
         </nav>
         {/* Form */}
-        <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:h-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-3xl mb-8 font-semibold">
-              {variant === "login" ? "Sign in" : "Register"}
+        <div className='flex justify-center'>
+          <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:h-2/5 lg:max-w-md rounded-md w-full'>
+            <h2 className='text-white text-3xl mb-8 font-semibold'>
+              {variant === 'login' ? 'Sign in' : 'Register'}
             </h2>
-            <div className="flex flex-col gap-4">
+            <div className='flex flex-col gap-4'>
               {/* Switch between Login or Register */}
-              {variant === "register" && (
+              {variant === 'register' && (
                 <Input
-                  label="Email"
+                  label='Email'
                   onChange={(ev: any) => setEmail(ev.target.value)}
-                  id="email"
-                  type="email"
+                  id='email'
+                  type='email'
                   value={email}
                 />
               )}
               <Input
-                label="Username"
+                label='Username'
                 onChange={(ev: any) => setName(ev.target.value)}
-                id="name"
-                type=""
+                id='name'
+                type=''
                 value={name}
               />
               <Input
-                label="Password"
+                label='Password'
                 onChange={(ev: any) => setPassword(ev.target.value)}
-                id="name"
-                type="password"
+                id='name'
+                type='password'
                 value={password}
               />
             </div>
-            <button className="bg-teal-100 py-3 text-teal-950  text-md font-semibold rounded-md w-full mt-10 hover:bg-green-400 transition">
-              {variant === "login" ? "Login" : "Sign Up"}
+            <button className='bg-teal-100 py-3 text-teal-950  text-md font-semibold rounded-md w-full mt-10 hover:bg-green-400 transition'>
+              {variant === 'login' ? 'Login' : 'Sign Up'}
             </button>
-            <div className="text-center">
-              <p className="text-neutral-500 mt-12 ">
-                {variant === "login"
-                  ? "First time using Netflix?"
-                  : "Already have an account?"}{" "}
+            <div className='text-center'>
+              <p className='text-neutral-500 mt-12 '>
+                {variant === 'login'
+                  ? 'First time using Netflix?'
+                  : 'Already have an account?'}{' '}
               </p>
               <span
                 onClick={toggleVariant}
-                className="text-white ml-1 hover:underline cursor-pointer"
+                className='text-white ml-1 hover:underline cursor-pointer'
               >
-                {variant === "login" ? "Create an Account" : "Sign In"}
+                {variant === 'login' ? 'Create an Account' : 'Sign In'}
               </span>
             </div>
           </div>
