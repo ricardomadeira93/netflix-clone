586012ee6d6f53a32d13adf7849252796b699897
ricardomadeira93 <ricardomadeira1993@gmail.com>
2023-07-21 16:27:16 +0100

    .

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -57,7 +57,7 @@ export const authOptions: AuthOptions = {
   },
   debug: process.env.NODE_ENV === 'development',
   adapter: PrismaAdapter(prismadb),
-  session: { strategy: 'jwt' },
+
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -68,7 +68,7 @@ const Auth = () => {
     }
   }, [email, name, password, login]);
 
-  return (
+  return (  
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
